33bc7e4a665d966672ff4e8d5ff6b6282a1d0580
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-04-14 17:09:34 -0300

    feat: working on api.go

--- api.go
+++ api.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type API struct {
 	addr string
@@ -10,12 +15,52 @@ func NewAddress(addr string) *API {
 	return &API{addr: addr}
 }
 
-func (a *API) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+// func WJson(w http.ResponseWriter, status int, v any) error {
+// 	w.WriteHeader(status)
+// 	w.Header().Set("Content-Type", "application/json")
+// 	return json.NewEncoder(w).Encode(v)
+// }
+
+// type apiFunc func(http.ResponseWriter, *http.Request) error
+
+// type ApiError struct {
+// 	Error string
+// }
+
+// func HttpHandleFunc(a apiFunc) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		if err := a(w, r); err != nil {
+// 			WJson(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+// 		}
+// 	}
+// }
+
+// func toGinHandler(h http.HandlerFunc) gin.HandlerFunc {
+// 	return func (c *gin.Context) {
+// 		h.ServeHTTP(c.Writer, c.Request)
+// 	}
+// }
+
+func (a *API) Run() {
+	router:= gin.Default()
+	router.Handle("GET", "/account", a.handleGetAccount)
+	// router.Handle("GET", "/account/{id}", toGinHandler(HttpHandleFunc(a.handleGetAccount)))
+	
+	log.Println("Listening on", a.addr)
+
+	router.Run(a.addr)
 }
 
-func (a *API) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+// func (a *API) handleAccount(w http.ResponseWriter, r *http.Request) error {
+// 	return nil
+// }
+
+func (a *API) handleGetAccount(c *gin.Context){
+	// id:= c.Param("id")
+
+	account:= NewAccount("Nathan", "Fabio")
+
+	c.IndentedJSON(http.StatusOK, account)
 }
 
 func (a *API) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {

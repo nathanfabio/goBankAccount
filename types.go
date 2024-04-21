75dd1afd10d62a4809e3147c6383d27eef3c9189
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-04-20 21:29:58 -0300

    feat: update structs

--- types.go
+++ types.go
@@ -1,22 +1,32 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
+
+type AccountRequest struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
 
 type Account struct {
-	ID         int `json:"id"`
-	FirstName  string `json:"firstName"`
-	LastName   string `json:"lastName"`
-	NumberBank int64  `json:"numberBank"`
-	Balance    float64 `json:"balance"`
+	ID         int       `json:"id"`
+	FirstName  string    `json:"firstName"`
+	LastName   string    `json:"lastName"`
+	NumberBank int64     `json:"numberBank"`
+	Balance    float64   `json:"balance"`
+	CreateAt   time.Time `json:"createAt"`
 }
 
 // NewAccount creates a new bank account
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(1000),
-		FirstName: firstName,
-		LastName:  lastName,
+		ID:         rand.Intn(1000),
+		FirstName:  firstName,
+		LastName:   lastName,
 		NumberBank: rand.Int63n(10000),
-		Balance:   0,
+		Balance:    0,
+		CreateAt:   time.Now().Local().UTC(),
 	}
 }

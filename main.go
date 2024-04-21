074cc1a58cf773eca2ce064e6ea8a4b68860ebbe
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-04-20 22:10:46 -0300

    feat: main.go update

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-
+import (
+	"log"
+)
 
 func main() {
-	server:= API{addr: ":8080"}
+	store, err := NewPostgresStorage()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := API{addr: ":8080", store: store}
 	server.Run()
 }
